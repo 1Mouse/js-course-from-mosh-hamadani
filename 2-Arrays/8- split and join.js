b13abc2eaadc385373ccc4243bc8d7cd9d52d24f
Mohamed Elfar <melfar391@gmail.com>
2022-03-01 06:19:17 +0200

    initial commit

---
+++ 2-Arrays/8- split and join.js
@@ -0,0 +1,10 @@
+const nums=[1,2,3]
+const joined=nums.join(',')
+console.log(joined)
+
+const message="this is a new message"
+const parts=message.split(' ')
+console.log(parts)
+
+const combined=parts.join('_')
+console.log(combined)
