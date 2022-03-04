df958d48a1e71c540e5a6cdf730eb71a09321c57
Mohamed Elfar <melfar391@gmail.com>
2022-03-04 14:00:14 +0200

    initial commit

---
+++ 3- functions/8- changing this using bind.js
@@ -0,0 +1,16 @@
+const video = {
+    title: 'avengers end game',
+    tags: ['action', 'adventure', 'thriller'],
+    showTags() {
+        // if we have a method that doesn't take a "this" as input
+        // we can change this bind
+        const self=this;
+        this.tags.forEach(function (tag) {// this --> object video
+            console.log(this.tags, tag);
+        }.bind(this));//this -> video as before we execute the callback global function "window"
+                      // we bind it to the object of the method "showTags" scope "video"
+    }
+};
+
+video.showTags();
+
